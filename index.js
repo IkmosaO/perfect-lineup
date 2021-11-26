60d0a9ab9ebefe17d12e96d7148a230a155933b4
Ikponmwosa <otasowieik@gmail.com>
2021-11-26 17:50:46 -0500

    added single team lineup condition

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ const validateLineup = (array) => {
   // is more than 45000, the lineup is rejected
   if (array.reduce((acc, player) => acc + player.salary, 0) > 45000) return false
   // compare team IDs, if each team is different change singleTeam to true
+  if (singleTeam(array)) {
+    singleTeam = true
+  }
 }
 
 // this function returns true if each teamid in a lineup is different
-// const singleTeam = (array) => {
-//   const uniqueElements = new Set()
-//   const orgininalTeamIds = array.filter()
-//   return uniqueElements.size === 
-// }
+const singleTeam = (array) => {
+  const uniqueElements = new Set()
 
-
-
+  return array.some(currentObject => uniqueElements.size === uniqueElements.add(currentObject.teamId).size)
+}
 
 // validateLineup(lineup)
 
