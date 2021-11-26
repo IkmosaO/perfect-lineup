741f26bd5c3dd28c4b49642fcb7bec15ad501313
Ikponmwosa <otasowieik@gmail.com>
2021-11-26 17:17:34 -0500

    added salary guard clause

---
+++ index.js
@@ -0,0 +1,34 @@
+/* 
+this function inputs an array of objects 
+
+this function either accepts or denies a list
+
+this function returns either true or false
+*/
+
+// const lineup = require('./tests')
+
+const validateLineup = (array) => {
+  let singleTeam = false
+  let singleGame = false
+  let playerPositions = false
+
+  // total salary of lineup guard clause, if the total salary of the players in a lineup
+  // is more than 45000, the lineup is rejected
+  if (array.reduce((acc, player) => acc + player.salary, 0) > 45000) return false
+  // compare team IDs, if each team is different change singleTeam to true
+}
+
+// this function returns true if each teamid in a lineup is different
+// const singleTeam = (array) => {
+//   const uniqueElements = new Set()
+//   const orgininalTeamIds = array.filter()
+//   return uniqueElements.size === 
+// }
+
+
+
+
+// validateLineup(lineup)
+
+module.exports = validateLineup
